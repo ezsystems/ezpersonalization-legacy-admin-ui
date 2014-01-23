e727691d013ae178e4ca891710478073c30082c5
alexander.tsikinovsky <alexander.tsikinovsky@localhost>
2014-01-23 17:03:46 +0200

    RSYS-1337 enrichment of the preview

---
+++ ebl/js/configurator_header.js
@@ -0,0 +1,134 @@
+
+var mandatorInfo;
+	  
+function initialize_configurator_header(callback, i18n_save_button) {
+
+	$.when(
+		$.get("/includes/configurator_header.html", '', function(data) {
+	
+			$("header.scenario_settings").html(data);
+	
+			if (i18n_save_button) {
+				$("#button_save").attr("data-translate", i18n_save_button);
+			}
+	
+			i18n($("header.scenario_settings")); // internationalize just loaded
+													// file
+	
+			$('.extended-config .delete').click(function() {
+				deleteScenario();
+			});
+	
+			$('.extended-config .cancel').click(function() {
+				cancelScenario();
+			});
+	
+			$('#toparrow').click(function() {
+				if ($(".extended-config").is(":visible")) {
+					$('.extended-config').hide();
+				} else {
+					$('.extended-config').show();
+				}
+			});
+		}), 
+		loadMandatorInfo(function(json) {
+			mandatorInfo = json;
+		})
+	).done(function(json) {
+		if (!mandatorInfo) {
+			alert('!!!');
+		}
+		if (callback) {
+			callback();
+		}
+	});
+}
+
+
+function loadMandatorInfo(callback) {
+	
+	var result = $.ajax({
+			dataType: "json",
+			beforeSend: function (req) {
+			req.setRequestHeader('no-realm', 'realm1');
+		},
+		url: "/api/v4/base/get_mandator/" + encodeURIComponent(customerID) + "?advancedOptions&itemTypeConfiguration",
+		success: callback,
+		error : function(jqXHR, textStatus, errorThrown) {
+			settingsDefaultErrorHandler(jqXHR, textStatus, errorThrown);
+        }
+	});
+	return result;
+}
+		
+
+function cancelScenario() {
+	  window.parent.$("#settingsP").hide();
+	  window.parent.$('#cover').hide();
+}
+
+
+function getItemTypeDescriptions(ids) {
+	var result = [];
+	for (var i in ids) {
+		result.push(getItemTypeDescription(ids[i]));
+	}
+	return result;
+}
+
+
+function getItemTypeDescription(id) {
+	for (var i in mandatorInfo.itemTypeConfiguration.types) {
+		var t = mandatorInfo.itemTypeConfiguration.types[i];
+		if (id == t.id) {
+			return  t.description + ' (' + t.id + ')';
+		}
+	}
+	return 'Unknown (' + t.id + ')';
+}
+
+
+function deleteScenario() {
+	
+	var fromTemplate = gup('from_template');
+	
+	if(fromTemplate) {
+		cancelScenario();
+		return;
+	}
+	  
+	var  translationConfirm = jQuery.i18n.prop("message_want_delete_scenario");
+	
+	if(!confirm(translationConfirm)) {
+		//do nothing
+	} else {
+	
+		url = "ebl/v3/"+customerID+"/structure/delete_scenario/";
+	
+		$.ajax({
+			type:"POST",
+			contentType: "application/json;charset=UTF-8",
+			data: JSON.stringify(reference_code),
+			url: url,
+			success: function(json){
+				//on success
+				window.parent.location = "index.html";
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				if(jqXHR.status != null && jqXHR.status == 403) {
+					setMessagePopUp("problem", "error_server_error_403");
+				} else if(jqXHR.status != null && jqXHR.status == 401) {
+					setMessagePopUp("problem", "error_server_error_401");
+				} else if(jqXHR.status != null && jqXHR.status == 400) {
+					setMessagePopUp("problem", "error_server_error_400");
+				} else if(jqXHR.status != null && jqXHR.status == 404) {
+					setMessagePopUp("problem", "error_server_error_404");
+				} else if(jqXHR.status != null && jqXHR.status == 409) {
+					setMessagePopUp("problem", "error_server_error_409");
+				} else {
+					setMessagePopUp("problem", "error_server_error");
+				}
+			}
+		});
+	}
+}
---
+++ ebl/js/preview.js
@@ -0,0 +1,292 @@
+ var reference_code = gup('reference_code');
+ var customerID = gup('customer_id');
+ var outputtypes = gup('outputtypes');
+ var inputtype = gup('inputtype');
+
+ 
+ $(document).ready(function() {
+		
+	  // load included files and call initializer
+	  initialize_configurator_header(function() {
+		  initialize();
+	  }, "preview_send");
+ });
+	
+ var initialize = function() {
+	 
+	  window.parent.$("#contentFrame").height( '100%');
+	  $('.settings_tab').find('a').attr('href', 'settingspop.html?reference_code=' + reference_code + '&customer_id=' + customerID);
+	  $('.configurator_tab').find('a').attr("href", "configuratorpop.html?reference_code=" + reference_code + "&customer_id=" + customerID);
+	 
+
+	  var outputArray = outputtypes.split(',');
+	  $('#output_type').children('option').each(function(index){
+		  if(index != 0){
+				if( $.inArray($(this).val(), outputArray) == -1){
+					$(this).hide();
+				}
+		  }
+		});
+	  
+
+	  $("#button_save").click(function() {
+		  call_recs();
+	  });
+	  
+	  $('#addItem').click(function() {
+		  addItem();
+		  getCallId();
+	  });
+	  $('#user_id').val('someuser');
+	  getCallId($('#user_id').val());
+	  $('#user_id').change(function(){
+		  getCallId();
+	  });
+	  $('#output_type').change(function(){
+		  getCallId();
+	  });
+	  $('#top_n').change(function(){
+		  getCallId();
+	  });
+	  initHelpBtn();
+	  
+	  $.ajax({
+		  dataType: "json",
+		  beforeSend: function(req) {
+			  req.setRequestHeader('no-realm', 'realm1');
+		  },
+		  url: "ebl/v3/" + customerID + "/structure/get_scenario/" + reference_code,
+		  success: function(json) {
+			  if(json.scenario.title == null || json.scenario.title == "") {
+				  $("#scenario_title").attr('placeholder',reference_code);
+				 
+			  }
+			  else {
+				 $("#scenario_title").attr('placeholder',json.scenario.title);
+			  }
+		  }
+		  });
+	 
+  };
+  
+  
+  /**
+   * retrieves a item from server with given Id
+   * @param id
+   * @returns {*}
+   */
+  function getItems(type, ids,jsonToshow) {
+	  $.ajax({
+		  dataType: "json",
+		  beforeSend: function(req) {
+			  req.setRequestHeader('no-realm', 'realm1');
+		  },
+		  url: "ebl/v4/" + customerID + "/elist/get_list_items/" + type + '/' + ids,
+		  success: function(json) {
+			var list = jsonToshow.recommendationResponseList;
+			for(var i in list){
+				var id = list[i].itemId;
+				for(var j in json){
+					var id2 = json[j].id;
+					if(id == id2){
+						console.log("found id ="+id);
+						var title = json[j].title;
+						if(title !=null){
+							list[i].title = title;
+						}
+						var price = json[j].price;
+						if(price !=null){
+							var amount = price.value;
+							if(amount != null){
+								list[i].price = amount+" "+price.currency;
+							}
+						}
+					 }
+				}	
+			}
+			console.log("json="+JSON.stringify(jsonToshow));
+			showJson(jsonToshow);
+		  }
+	 });
+  };
+ 
+  function showWithTitles(json){
+		var list = json.recommendationResponseList;
+		var it = inputtype;
+		if(it == null || it == ''){
+			it = 1;
+		}
+		var ids ='';
+		for(var i in list){
+			var id = list[i].itemId;
+			ids += id+",";
+		}
+		if(ids.length > 0){
+			ids = ids.substring(0, ids.length-1);
+		}
+		getItems(it, ids, json);
+	}
+  
+  function addItem(){
+	  var id = $('#itemId').val();
+	  if(! id.length) {
+		  showError('editorial_list_error_empty_id_field');
+		  return;
+	  }
+	  //test if the id contains just digits
+	  if(/\D/.test(id)) {
+		  //we have non digits in the field
+		  showError('editorial_list_error_invalid_id');
+		  return;
+	  }
+	  id = parseInt(id, 10);
+	  if(id > 2147483647 || id < 0) {
+		  showError('editorial_list_error_id_out_of_bounds');
+		  return;
+	  }
+	  $('.validation_message').hide();
+	  var items = $('#context_items');
+	  if(items.val()!=null && items.val()!=''){
+		  items.val(items.val()+','+id); 
+	  }else{
+		  items.val(id);  
+	  }
+	  $('#itemId').val('');
+  }
+  
+  function showError(error){
+	  $('.error').attr('data-translate', error);
+	  $('.validation_message').show();
+	  localizer();
+  }
+  
+  function getCallId(){
+		var url = getCallSrc().replace('jsonp','json');
+		$('#urlid').text(url);
+  }
+  
+  function getCallSrc(){
+	  	var userId = $('#user_id').val();
+	  	var outputtype = $('#output_type').val();
+	  	var topn = $('#top_n').val();
+		var customerID = $.cookie('customerID');
+		var cur_host = window.location.host;
+		var dev = 'cat.dev';
+		var dev2 = 'development';
+	    var test = '.test.';
+	    var recoHost ='';
+	    console.log(cur_host);
+		if(cur_host.indexOf(dev)!=-1 ){
+			recoHost = 'http://cat.development.yoochoose.com:8080/recocontroller';
+		}else{
+			if(cur_host.indexOf(dev2)!=-1 ){
+				recoHost = 'http://admin.development.yoochoose.com:8080/recocontroller';
+			}else{
+				if(cur_host.indexOf(test)!=-1 ){
+					recoHost = 'https://reco.test.yoochoose.net';
+				}else{
+					recoHost = 'https://reco.yoochoose.net';
+				}
+			}
+		}
+		var scriptSrc = recoHost+'/ebh/'+customerID+'/'+userId+'/'+reference_code+'.jsonp';
+		var items = $('#context_items');
+		
+		if(items.val()!=null && items.val()!=''){
+			scriptSrc += '?contextitems='+items.val()+'&numrecs='+topn; 
+		}else{
+			scriptSrc += '?numrecs='+topn; 
+		}
+		if(outputtype > 0){
+			scriptSrc += '&outputtype='+outputtype;
+		}
+		return scriptSrc;
+  }
+  
+  function call_recs(){
+		
+		
+		var scriptSrc = getCallSrc();
+		var heads = document.getElementsByTagName('head');
+		var head = null;
+		if(heads != null){
+			head = heads[0];
+		}
+		
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = scriptSrc;
+		if(head != null){
+			head.appendChild(script);
+		}else{
+			if(typeof(document.readyState) == 'undefined' ||
+					document.readyState == 'complete' ||
+					document.readyState == 'loaded')
+			{
+				var body = document.getElementsByTagName('body')[0];
+				body.insertBefore(script, body.lastChild);
+			}
+			else
+			{
+				var id = new Date().getMilliseconds();
+				document.write('<p id="ycscrp'+id+'" style="display:none;"></p>');
+				document.getElementById('ycscrp'+id).insertBefore(script, null);
+			}
+		}
+	}
+  
+function jsonpCallback(json){
+	showJson(json);
+	showWithTitles(json);
+	
+}
+
+
+
+function showJson(json){
+	var strjs = JSON.stringify(json,null,'&nbsp;');
+	strjs = strjs.replace(/,/g , ",<br/>");
+	strjs = strjs.replace(/}/g , "<br/>}");
+	strjs = strjs.replace(/{/g , "{<br/>");
+	strjs = strjs.replace("[" , "[<br/>");
+	strjs = strjs.replace("]" , "<br/>]");
+	
+	$("#prettyprint").html(strjs);
+	$("#prettyprint").removeClass("prettyprinted"); 
+	prettyPrint();
+}
+
+
+
+  
+
+  //ajax request for the right section
+
+
+
+  function stdAjaxErrorHandler(jqXHR, textStatus, errorThrown) {
+	  if(jqXHR.status != null && jqXHR.status == 403) {
+		  setMessagePopUp("problem", "error_server_error_403");
+	  }
+	  else if(jqXHR.status != null && jqXHR.status == 401) {
+		  setMessagePopUp("problem", "error_server_error_401");
+	  }
+	  else if(jqXHR.status != null && jqXHR.status == 400) {
+		  setMessagePopUp("problem", "error_server_error_400");
+	  }
+	  else if(jqXHR.status != null && jqXHR.status == 404) {
+		  setMessagePopUp("problem", "error_server_error_404");
+	  }
+	  else if(jqXHR.status != null && jqXHR.status == 409) {
+		  setMessagePopUp("problem", "error_server_error_409");
+	  }
+	  else {
+		  setMessagePopUp("problem", "error_server_error");
+	  }
+  }
+
+
+
+
+ 
+
