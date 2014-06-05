b6cb912ec25c0aaa5d58cb024afc5b1b2705a33d
alexander.tsikinovsky <alexander.tsikinovsky@localhost>
2014-06-05 19:11:10 +0300

    RSYS-1503 forgot password update

--- ebl2/js/register.js
+++ ebl2/js/register.js
@@ -67,6 +67,121 @@
 		unsetWaitPopUp();
 	}
 	
+
+	/** Sets password reset div into "finished" mode. */
+	function resetPasswordFinished() {
+		showMessageFP('reset_password_message_email_sent');
+		
+		$('#messageReset input').attr('readonly', true);
+		
+	    $("#messageReset .close").show();
+	    $("#messageReset li.captcha").hide();
+	    $("#messageReset .send").hide();
+	}
+	
+	function showMessageFP(msg){
+		$('#validation_message').attr('data-translate', msg);
+		localizer();
+		$('#validation_messageli').show();
+	}
+	
+	function resetpass () {
+
+	    var captchascope = Math.round(Math.random() * 100000);
+
+	    $('#fpcaptchaimage').attr('src', '/ebl/v3/registration/create_captcha?captchascope=' + captchascope);
+		
+	    $('#fplogin').off('click').on('click',function () {
+	    	var showError = false;
+	    	var mailProblem = false;
+	        var fpcaptcha = $('#fpcaptcha').val();
+			var fpemail = $('#fpemail').val();
+			var isEbl2 = 'ebl2';
+	        var url = '/ebl/v3/registration/forgot_password';
+	        if(fpemail == "")  {
+	        	$('#fpemail').parent().addClass("problem");
+	        	showError = true;
+	        } else {
+	        	
+	        	if(fpemail.length<5 || fpemail.indexOf('@') == -1 || fpemail.indexOf('.') == -1 ){
+	        		$('#fpemail').parent().addClass("problem");
+	        		mailProblem = true;
+	        	}else{
+	        		$('#fpemail').parent().removeClass("problem");
+	        	}
+	        }
+	        if(fpcaptcha == "") {
+	        	$('#fpcaptcha').parent().addClass("problem");
+	        	showError = true;
+	        } else {
+	        	$('#fpcaptcha').parent().removeClass("problem");
+	        }
+	        
+	        if(showError || mailProblem){
+	        	var msg = '';
+	        	if(mailProblem){
+	        		msg = "error_wrongmail";
+	        	}else{
+	        		msg = "error_fill_required_fields";
+	        	}
+	        	showMessageFP(msg);
+	        		
+	        } else {
+	            
+	            $.ajax({
+	                type: "POST",
+	                dataType: "json",
+	                data: {
+	                    username: fpemail,
+	                    captcha: fpcaptcha,
+	                    ebl2: isEbl2,
+	                    captchascope: captchascope
+	                },
+	                url: url,
+	                statusCode: {
+	                    412: function (jqXHR, textStatus, errorThrown) {
+	        	        	showMessageFP("reset_password_message_captcha_not_correct");
+							captchascope = Math.round(Math.random() * 100000);
+							$('#captchaimage').attr('src', '/ebl/v3/registration/create_captcha?captchascope=' + captchascope);
+							$('#captcha').val("");
+	                    }
+	                },
+	                success: function (json) {
+	                    //on success
+	                	resetPasswordFinished();
+	                },
+	                error: function (jqXHR, textStatus, errorThrown) {
+	                   if(jqXHR.status != null && jqXHR.status == 403)
+						{
+	                	   showMessageFP("error_server_error_403");
+						}
+						else if(jqXHR.status != null && jqXHR.status == 401)
+						{
+							showMessageFP("error_server_error_401");
+						}
+						else if(jqXHR.status != null && jqXHR.status == 400)
+						{
+							showMessageFP("error_server_error_400");
+						}
+						else if(jqXHR.status != null && jqXHR.status == 404)
+						{
+							showMessageFP("error_server_error_404");
+						}
+						else if(jqXHR.status != null && jqXHR.status == 409)
+						{
+							showMessageFP("error_server_error_409");
+						}
+						else
+						{
+							showMessageFP("error_server_error");
+						}
+	                }
+	            });
+	         }
+	    });
+	    
+	}
+	
 $(document).ready(function () {
 	
 	
@@ -115,6 +230,13 @@ $(document).ready(function () {
 
 	var emaildecode = decodeURIComponent(email);
 	$('#email').val(emaildecode);
+	
+	var editData =$("#reset_password");
+	   editData.on("click", function(event){
+		   setDialogsContact('messageReset','messageBodyReset','destroy_dialog','fpclose');
+		   resetpass();
+	   });
+	
 	if(lang != "")
 	{
 		$.cookie('language', lang, { expires: 365 });
