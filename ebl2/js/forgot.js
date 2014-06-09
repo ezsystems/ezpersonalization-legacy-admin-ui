00d805b78e4bcb13a86c09bd08cb681793240669
alexander.tsikinovsky <alexander.tsikinovsky@localhost>
2014-06-09 11:38:29 +0300

    hg flow, merge release `3.1.314` to develop branch `develop`

--- ebl2/js/forgot.js
+++ ebl2/js/forgot.js
@@ -32,97 +32,108 @@ function setAnnotation(msg){
 	$('.validation_message').show();
 }
 
+function changePass() {
+
+	var newPassword = $('#password').val();
+	var passwordConfirm = $('#password_confirm').val();
+	if(newPassword == null || newPassword.length < 8 ){
+		$('#password').parent().addClass('problem');
+		setAnnotation("edit_contact_data_password_length");
+	}else if(newPassword != passwordConfirm){
+		$('#password').parent().removeClass('problem');
+		$('#password_confirm').parent().addClass('problem');
+		setAnnotation("edit_contact_data_confirmation_does_not_match");
+	} else {
+		$('#password').parent().removeClass('problem');
+		$('#password_confirm').parent().removeClass('problem');
+		$('.validation_message').hide();
+		setWaitPopUp();
+		var url = $('form').attr('action');
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			data: {
+				username: username,
+				code: code
+			},
+			url: url,
+			statusCode: {
+				400: function (jqXHR, textStatus, errorThrown) {
+					unsetWaitPopUp();
+					setAnnotation("reset_password_message_invalid_forgotten_password_code");
+				},
+			},
+			success: function (json) {
+
+				//The code was correct and the passwort can be changed.
+				$.ajax({
+					type: "POST",
+					beforeSend: function (req) {
+						req.setRequestHeader('Authorization', make_base_auth(username, code));
+						req.setRequestHeader('no-realm', 'realm1');
+					},
+					mimeType: "application/json",
+					contentType: "application/x-www-form-urlencoded",
+					dataType: "json",
+					data: {
+						password: newPassword
+					},
+					url: "ebl/v3/profile/change_password",
+					success: function (json) {
+
+						$.ajax({
+							type: "POST",
+							url: "/ebl/v3/registration/create_access_token",
+
+							beforeSend: function (req) {
+								req.setRequestHeader('Authorization', make_base_auth(username, password));
+								req.setRequestHeader('no-realm', 'yes');
+							},
+							dataType: "json",
+							data: {
+								set_cookie: true,
+								login: username,
+								password :newPassword
+							},
+							success: function (json) {
+								$.cookie('email', username);
+								unsetWaitPopUp();
+								setAnnotation("reset_password_message_password_set");
+								$('#validation_message2').css( "color", "green");
+								$('#message_text').delay(3000).queue(function() {
+									window.location = "main.html";
+								});
+							},
+							error: error_handler
+						});
+						//on success
+
+
+
+
+					},
+					error: error_handler
+				});
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				unsetWaitPopUp();
+				setAnnotation("error_server_error");
+			}
+		});
+	}
+}
+
 $(document).ready(function () {
 
 
 	$('a.login').click(function () {
-
-		var newPassword = $('#password').val();
-		var passwordConfirm = $('#password_confirm').val();
-		if(newPassword == null || newPassword.length < 8 ){
-			$('#password').parent().addClass('problem');
-			setAnnotation("edit_contact_data_password_length");
-		}else if(newPassword != passwordConfirm){
-			$('#password').parent().removeClass('problem');
-			$('#password_confirm').parent().addClass('problem');
-			setAnnotation("edit_contact_data_confirmation_does_not_match");
-		} else {
-			$('#password').parent().removeClass('problem');
-			$('#password_confirm').parent().removeClass('problem');
-			$('.validation_message').hide();
-			setWaitPopUp();
-			var url = $('form').attr('action');
-			$.ajax({
-				type: "POST",
-				dataType: "json",
-				data: {
-					username: username,
-					code: code
-				},
-				url: url,
-				statusCode: {
-					400: function (jqXHR, textStatus, errorThrown) {
-						unsetWaitPopUp();
-						setAnnotation("reset_password_message_invalid_forgotten_password_code");
-					},
-				},
-				success: function (json) {
-
-					//The code was correct and the passwort can be changed.
-					$.ajax({
-						type: "POST",
-						beforeSend: function (req) {
-							req.setRequestHeader('Authorization', make_base_auth(username, code));
-							req.setRequestHeader('no-realm', 'realm1');
-						},
-						mimeType: "application/json",
-						contentType: "application/x-www-form-urlencoded",
-						dataType: "json",
-						data: {
-							password: newPassword
-						},
-						url: "ebl/v3/profile/change_password",
-						success: function (json) {
-
-							$.ajax({
-								type: "POST",
-								url: "/ebl/v3/registration/create_access_token",
-
-								beforeSend: function (req) {
-									req.setRequestHeader('Authorization', make_base_auth(email, password));
-									req.setRequestHeader('no-realm', 'yes');
-								},
-								dataType: "json",
-								data: {
-									set_cookie: true,
-									login: username,
-									password :newPassword
-								},
-								success: function (json) {
-									$.cookie('email', email);
-									unsetWaitPopUp();
-									setAnnotation("reset_password_message_password_set");
-									$('#validation_message2').css( "color", "green");
-									$('#message_text').delay(3000).queue(function() {
-										window.location = "main.html";
-									});
-								},
-								error: error_handler
-							});
-							//on success
-
-
-
-
-						},
-						error: error_handler
-					});
-				},
-				error: function (jqXHR, textStatus, errorThrown) {
-					unsetWaitPopUp();
-					setAnnotation("error_server_error");
-				}
-			});
-		}
+			changePass();
 	});
+	
+	$('#password_confirm').keypress(function (e) {
+	        if (e.which == 13) {
+	            $(this).blur();
+	            changePass();
+	        }
+	 });
 });
