9d977c04ce9a85ad63416b9fc028fa9834cd98e3
rodion.alukhanov <rodion.alukhanov@localhost>
2014-07-03 09:04:54 +0200

    RSYS-1526

---
+++ ebl/js/dao/mandator.js
@@ -0,0 +1,38 @@
+
+
+
+/** Scenario DAO service. It loads and updates single scenarios.<br><br>
+ * 
+ */
+var mandatorDao = {
+	mandator: null,
+	error:null
+};
+	
+
+
+mandatorDao.init = function(mandatorId, callback) {
+	
+	if (!mandatorId) {
+		return;
+	}
+	
+	var result = $.ajax({
+		dataType: "json",
+		url: "/api/v4/base/get_mandator/" + encodeURIComponent(mandatorId) + "?advancedOptions&itemTypeConfiguration&no-realm",
+		success: function(json) {
+			mandatorDao.mandator = json;
+			if (callback) {
+				callback(json);
+			}
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			mandatorDao.error = {
+				message: "Error loading mandator [" + mandatorId + "]",
+				status:  jqXHR.status
+			};
+        }
+	});
+	return result;
+};
+
