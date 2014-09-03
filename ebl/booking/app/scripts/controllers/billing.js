afdf547ad0bc262e4e2d3c09f2d1f5a243103b0f
roland.gude <roland.gude@localhost>
2014-09-03 14:15:46 +0200

    RSYS-1569
    scaffolding from yeoman

---
+++ ebl/booking/app/scripts/controllers/billing.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ycBookingApp.controller:BillingCtrl
+ * @description
+ * # BillingCtrl
+ * Controller of the ycBookingApp
+ */
+angular.module('ycBookingApp')
+  .controller('BillingCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
