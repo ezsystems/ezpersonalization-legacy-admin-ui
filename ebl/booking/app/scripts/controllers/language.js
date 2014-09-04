5eab0f3e20361d9b603fe72ba04e99691b6e291f
roland.gude <roland.gude@localhost>
2014-09-04 15:26:55 +0200

    RSYS-1569
    flag sprites

--- ebl/booking/app/scripts/controllers/language.js
+++ ebl/booking/app/scripts/controllers/language.js
@@ -9,7 +9,7 @@
  */
 angular.module('ycBookingApp')
   .controller('LanguageCtrl', function ($scope, $translate) {
-      $scope.languages = ['en', 'de'];
+      $scope.languages = [{code: 'en', flag: 'us'}, {code: 'de', flag:'de'}];
       $scope.changeLanguage = function (langKey) {
           console.log("switch language to ", langKey);
           $translate.use(langKey);
