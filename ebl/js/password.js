fa13df9206d57717f4bd012c6a2992d0be64614d
rodion.alukhanov <rodion.alukhanov@localhost>
2014-10-21 13:25:42 +0200

    RSYS-1570

--- ebl/js/password.js
+++ ebl/js/password.js
@@ -1,32 +1,18 @@
 
 
-
+var temporaryToken = false;
 
 
 $(document).ready(function () {
 
 	yooAjax('#password_dialog', {
-		 url: "/api/v4/profile/get_me",
-         data: {
-     		"email" : email
-     	 },
+		 url: "/api/v4/profile/get_me?withAuthenticationInformation",
 		 success: function (json) {
-			 switchState("login", true);
-			 $("#login_dialog input[name='password']").val("");
-			 localMessage("positive", "reset_password_message_email_sent");
+			 var temporaryToken = json.authenticationInformation.temporary;
+			 if (temporaryToken) {
+				 $("#password_old").hide();
+			 }
 		 },
-		 fault_emailFault: function(json) {
-			 localMessage("negative", "login_fault_email", email); return;
-		 },
-		 fault_loginNotFoundFault: function(json) {
-			 localMessage("negative", "login_fault_login_not_found", email); return;
-		 },
-		 fault_userDisabledFault: function(json) {
-			 localMessage("negative", "login_fault_user_disabled", email); return;
-		 },
-		 fault: function(json) {
-			 localMessage("negative", "login_fault_unexpected", json.faultCode, JSON.stringify(json.faultDetail));
-		 },			 
 		 error: function (jqXHR, textStatus, errorThrown) {
 			 localMessage("negative", "error_server_error", jqXHR.status);
 		}
