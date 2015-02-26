221e2c6d154ba927a7734bac39a094e1f0cbebd7
johannes.hoerle <johannes.hoerle@localhost>
2015-02-26 14:49:14 +0100

    Merge with develop

--- ebl/js/added_revenue.js
+++ ebl/js/added_revenue.js
@@ -5,36 +5,100 @@
 
 function loadAddedRevenue() {
 	
-	setLoadingDiv('#addedRevenue');
-	
 	var from_date_time = currentPeriodFromTime();
 	var to_date_time = currentPeriodToTime();
+	
+	var mandator = mandatorDao.mandator; // mandatorDao is global
+	
+	$('#addedRevenue tr:not(.fixed)').slice(1).remove();
+	$('#addedRevenue tr.overflow').hide();
+	$('#addedRevenue tr.nothing').hide();
+	
+	if (! mandator) {
+		return; // something went wrong
+	}
+	
+	$('#addedRevenue span.currency').text("(" + mandator.advancedOptions.currency + ")");
+	
+	setLoadingDiv('#addedRevenue');
+	
+	var limit = 5;
 
 	yooAjax(null, {
-		url: "/api/v4/" + encodeURIComponent(customerID) + "/statistic/added_revenue?from_date_time=" + from_date_time + "&to_date_time=" + to_date_time,
+		url: "/api/v4/" + encodeURIComponent(customerID) + "/statistic/added_revenue?limit="+(limit + 1)+"&from_date_time=" + from_date_time + "&to_date_time=" + to_date_time,
 		success: function (json) {
+
+			var index = 0;
 			
 			for (var i in json) {
+				
+				if (index >= limit) {
+					$('#addedRevenue tr.overflow').show();
+					$('#addedRevenue span.limit').text(limit);
+					break;
+				}
+				
 				var e = json[i];
+				
+				var date = dateFormat(e.timeConsumed) + " [" + dateDiffMinutes(e.timeRecommended, e.timeConsumed) + " minutes]";
 			
-				$('#addedRevenue tr:nth-child(2) td.reco_accepted').text(e.timeRecommended + "->" + e.timeConsumed);
+				$('#addedRevenue tr:nth-child(2) td.reco_accepted').text(date);
 				$('#addedRevenue tr:nth-child(2) td.item_bought').text(e.item.id + ":" + e.item.title);
 				if (e.price) {
+					var price = e.price.toFixed(mandator.advancedOptions.currencyFractionDigits);
+					
 					if (e.quantity && e.quantity > 1) {
-						$('#addedRevenue tr:nth-child(2) td.item_price').text(e.quantity + "x" + e.price + e.currency);	
+						$('#addedRevenue tr:nth-child(2) td.item_price').text(e.quantity + " x " + price);	
 					} else {
-						$('#addedRevenue tr:nth-child(2) td.item_price').text(e.price + e.currency);
+						$('#addedRevenue tr:nth-child(2) td.item_price').text(price);
 					}
 				}
 				
-				
 				var content = $('#addedRevenue tr:nth-child(2)').html();
-				$('#addedRevenue').append("<tr>" + content + "</tr>");
+				$('#addedRevenue table').append("<tr>" + content + "</tr>").show();
+				
+				index++;
 			}
-
+			
+			if (index == 0) {
+				$('#addedRevenue tr.nothing').show();
+				$('#addedRevenue a.create_csv').hide();
+			} else {
+				$('#addedRevenue a.create_csv').show();
+				$('section.scenarios a.create_csv').attr('href',"/api/" + encodeURIComponent(customerID) + "/statistic/added_revenue.xlsx?from_date_time=" + from_date_time + "&to_date_time=" + to_date_time);
+			}
+			
+			i18n($('#addedRevenue'));
 		}
 	}).always(function() {
 		unsetLoadingDiv('#addedRevenue');		
 	});
+	
 
+}
+
+
+function dateDiffMinutes(a, b) {
+	  if (! (a instanceof Date)) {
+		  a = new Date(a);
+	  }
+	  if (! (b instanceof Date)) {
+		  b = new Date(b);
+	  }
+	  var r = (b - a) / 1000 / 60;
+	  
+	  if (r > 0) {
+		  return Math.ceil(r);
+	  } else {
+		  return Math.floor(r);
+	  }
+}
+
+
+function dateFormat(date) {
+  if (! (date instanceof Date)) {
+	  date = new Date(date);
+  }
+
+  return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
 }
--- ebl/js/maininx.js
+++ ebl/js/maininx.js
@@ -63,18 +63,6 @@ $(document).ready(function() {
 	    renderRecommendationChart();
 	});
 	
-	//Last day click event
-	$('#view_option_day').click(function () {
-	    ajaxScenarioList("24H");
-	});
-	
-	$('#view_option_week').click(function () {
-		ajaxScenarioList("WEEK");
-	});
-	
-	$('#view_option_month').click(function () {
-	    ajaxScenarioList('MONTH');
-	});
 	
 	
 	// SCENARIOS, REVENUE, ABTESTS, IMPORT
@@ -163,14 +151,13 @@ $(document).ready(function() {
 					return;
 				}
 				
-				$.when(
-					mandatorDao.init(customerID),
-					ajaxScenarioList(period) // do not changing the period. It was just loaded from the session storage
-			    ).always(function() {
-			    	initialize().always(function() {
-		    			unsetLoadingDiv('section.mandant > header');		
+				mandatorDao.init(customerID).always(function() {
+					ajaxScenarioList(period).always(function() {
+						initialize().always(function() {
+							unsetLoadingDiv('section.mandant > header');
+						});
 			    	});
-			    });		
+				});
 			}
 		}).always(function() {
 			unsetLoadingDiv('section.mandant > header');		
@@ -190,26 +177,40 @@ function switchTab(newTab) {
 		newTab = sessionStorage.getItem("active_tab");
 	}
 	
-	$("section.scenarios div.tabContent, section.scenarios div.controls").hide();
+	$("section.scenarios ul.options_menu li").removeClass("current");
+	$("section.scenarios .tabContent").hide();
+	$("section.scenarios div.controls").hide();
 	
 	if (newTab == "SCENARIOS") {
 		$("#available_scenarios").show();
-	}
-	
-	if (newTab == "REVENUE") {
+		$("#scenarioControls").show();
+		$("section.scenarios li.tabScenarios").addClass("current");	
+		
+		
+	} else if (newTab == "REVENUE") {
 		$("#addedRevenue").show();
-	}
-	
-	if (newTab == "ABTESTS") {
+		$("#addedRevenueControls").show();
+		$("section.scenarios li.tabRevenue").addClass("current");
+		
+		
+	} else if (newTab == "ABTESTS") {
 		$("#ABTests").show();
+		$("#abControls").show();
+		$("section.scenarios li.tabAbTests").addClass("current");
 		
 		if(!$('#testList').data('tests').length){
 			$('#abControls').find('.helpLink').trigger('click');
 		}
-	}
-	
-	if (newTab == "IMPORT") {
+		
+		
+	} else if (newTab == "IMPORT") {
 		$("#importJobs").show();
+		$("#itemImortControls").show();
+		$("section.scenarios li.tabImports").addClass("current");
+		
+		
+	} else {
+		switchTab("SCENARIOS");
 	}
 }
 
@@ -387,6 +388,45 @@ function piwikCaller(name){
  *  @see #initialLoadData()
  */
 function initialize() {
+	
+	
+	//Last day click event
+	$('#view_option_day').click(function () {
+	    ajaxScenarioList("24H");
+	});
+	
+	$('#view_option_week').click(function () {
+		ajaxScenarioList("WEEK");
+	});
+	
+	$('#view_option_month').click(function () {
+	    ajaxScenarioList('MONTH');
+	});
+	
+	
+	// SCENARIOS, REVENUE, ABTESTS, IMPORT
+	$('section.scenarios li.tabScenarios').click(function() {
+		switchTab("SCENARIOS");
+	});
+	
+	
+	$('section.scenarios li.tabRevenue').click(function() {
+		switchTab("REVENUE");
+	});
+	
+	$('section.scenarios li.tabAbTests').click(function() {
+		switchTab("ABTESTS");
+	});
+	
+	$('section.scenarios li.tabImports').click(function() {
+		switchTab("IMPORT");
+	});
+	
+	
+	$('#createNewTest').click(function(){
+		gui.editTest(new Test(emptyTest));
+	});
+
 
 	var userLoadingPromise = getCurrentUser(function(loginInfo) {
 		
@@ -805,6 +845,7 @@ function initialLoadData() {
 	$('section.scenarios ul.options_menu').find('li:visible').removeClass('last-child');
 	$('section.scenarios ul.options_menu').find('li:visible:last').addClass('last-child');
  
+	switchTab(false); // switching tab loading the last tab from session storage
 }
 
 
