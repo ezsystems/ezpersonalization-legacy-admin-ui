1bb359ae5557b746d663f6da9b712a763da72f13
alexander.tsikinovsky <alexander.tsikinovsky@localhost>
2015-03-08 16:55:19 +0200

    RSYS-1626 continue developing gui nl2go

--- ebl/js/nl2go.js
+++ ebl/js/nl2go.js
@@ -41,6 +41,57 @@ function inlineSpectrumBodyBackground(){
 }
 
 function savePreferences() {
+	setLoadingDiv($('body'));
+	 retObj = new Object();
+	 retObj.mandatorName = customerID;
+	 retObj.logo = $("#logo").val();
+	 retObj.background = currentbg;
+	 retObj.facebook = $("#facebook").val();
+	 retObj.googleplus = $("#googleplus").val();
+	 retObj.twitter = $("#twitter").val();
+	 $.ajax({
+		  type: "POST",
+		  mimeType: "application/json",
+		  contentType: "application/json;charset=UTF-8",
+		  dataType: "html",
+		  data: JSON.stringify(retObj),
+		  url: "/api/v4/" + encodeURIComponent(customerID) + "/nl2go/get_preview_with_params",
+		  success: function(json) {
+			  unsetLoadingDiv($('body'));
+			  $('#previewFrame').contents().find('html').html(json);
+		  },
+		  error: function() {
+			  unsetLoadingDiv($('body'));
+			  stdAjaxErrorHandler();
+		  }
+	  });	
+}
+
+function stdAjaxErrorHandler(jqXHR, textStatus, errorThrown) {
+	  if(jqXHR != null && jqXHR.status == 403) {
+		  setMessagePopUp("problem", "error_server_error_403");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 401) {
+		  setMessagePopUp("problem", "error_server_error_401");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 400) {
+		  setMessagePopUp("problem", "error_server_error_400");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 404) {
+		  setMessagePopUp("problem", "error_server_error_404");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 409) {
+		  setMessagePopUp("problem", "error_server_error_409");
+	  }
+	  else {
+		  setMessagePopUp("problem", "error_server_error");
+	  }
+}
+
+
+
+
+function savePreferences2() {
 	alert('hehe');
 	setLoadingDiv($('body'));
 	 retObj = new Object();
