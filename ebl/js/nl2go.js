a428ce78b6ee69596769981e32b968c7bfbf6eab
johannes.hoerle <johannes.hoerle@localhost>
2015-03-17 11:05:33 +0100

    hg flow, merge release `3.2.1000` to develop branch `develop`

---
+++ ebl/js/nl2go.js
@@ -0,0 +1,274 @@
+var customerID = gup('customer_id');
+var currentbg = "#cccccc";
+var menuCurrentbg = "#003300";
+var ctaCurrentbg = "#123456";
+var topicCurrentbg = "#cccccc";
+var footerCurrentbg = "#111111";
+
+$(document).ready(function () {
+	inlineSpectrumBodyBackground();
+	
+	$('#import_primary_settings').submit(function () {
+		savePreferences2();
+		return false;
+    });
+	var isrc="/api/v4/" + encodeURIComponent(customerID) + "/nl2go/get_preview";
+	$('#previewFrame').attr('src',isrc);   
+	
+	$("#button_save").click(submitImport);
+	$(function() {
+	    $( "#accordion" ).accordion();
+	});
+});
+
+function submitImport() {
+	$('<input type="submit">').hide().appendTo($('#import_primary_settings')).click().remove();
+}
+
+function inlineSpectrumBodyBackground(){
+	 $.ajax({
+		  type: "GET",
+		  mimeType: "application/json",
+		  contentType: "application/json;charset=UTF-8",
+		  dataType: "json",
+		  url: "/api/v4/" + encodeURIComponent(customerID) + "/nl2go/pref/getMailPreferences",
+		  success: function(retObj) {
+			  unsetLoadingDiv($('body'));
+			  $("#logo").val(retObj.logo);
+			  $("#menu_elment_hotline_name").val(retObj.holineName);
+			  $("#menu_elment_hotline_number").val(retObj.holineNumber);
+			  $("#cta").val(retObj.cta);
+			  $("#topic").val(retObj.topic);
+			  
+			  if(retObj.background != null){
+				  currentbg = retObj.background;
+			  }
+			  if(retObj.menuBackground != null){
+				  menuCurrentbg = retObj.menuBackground;
+			  }
+			  if(retObj.ctaBackground != null){
+				  ctaCurrentbg = retObj.ctaBackground;
+			  }
+			  if(retObj.footerBackground != null){
+				  footerCurrentbg = retObj.footerBackground;
+			  }
+			  
+			  if(retObj.menuLinks != null){
+				  var kk = 1;
+				  var menuLinks = retObj.menuLinks;
+				  for(var menuLink in menuLinks){
+					  $("#menu_elment_name"+kk).val(menuLinks[menuLink].caption);
+					  $("#menu_elment_url"+kk).val(menuLinks[menuLink].url);
+					  kk++;
+				  }
+			  }
+			  $("#facebook").val(retObj.facebook);
+			  $("#googleplus").val(retObj.googleplus);
+			  $("#twitter").val(retObj.twitter);
+			  
+			  if(retObj.imprint != null){
+				  $("#imprintName").val(retObj.imprint.caption);
+				  $("#imprintURL").val(retObj.imprint.url);
+			  }
+			  
+			  if(retObj.unsubscribe != null){
+				  $("#unsubscribeName").val(retObj.unsubscribe.caption);
+				  $("#unsubscribeURL").val(retObj.unsubscribe.url);
+			  }
+			  $("#basket_subject").val(retObj.hello1);
+			  $("#basket_note").val(retObj.note1);
+			  $("#basket_footer").val( retObj.footer1);
+			  $("#click_subject").val(retObj.hello2);
+			  $("#click_note").val(retObj.note2);
+			  $("#click_footer").val(retObj.footer2);
+			 
+			  
+			  spectrumBackground('mailBackground',currentbg);
+			  spectrumBackground('menuBackground',menuCurrentbg);
+			  spectrumBackground('ctaBackground',ctaCurrentbg);
+			  spectrumBackground('footerBackground',footerCurrentbg);
+		  },
+		  error: function() {
+			  unsetLoadingDiv($('body'));
+			  spectrumBackground('mailBackground',currentbg);
+			  spectrumBackground('menuBackground',menuCurrentbg);
+			  spectrumBackground('ctaBackground',ctaCurrentbg);
+			  spectrumBackground('footerBackground',footerCurrentbg);
+			  stdAjaxErrorHandler();
+		  }
+	  });	
+	
+	
+	
+}
+
+function spectrumBackground(elementId, colorVariable){
+	$("#"+elementId).spectrum({
+	    showInput: true,
+	    showAlpha: true,
+	    color: colorVariable,
+	    move: function(color) {
+	    },
+	    change: function(color) {
+	    	var bgcolorPrev = color.toHexString(); 
+	    	if('mailBackground' === elementId ){
+	    		currentbg = bgcolorPrev;
+	    	}else if('menuBackground'  === elementId){
+	    		menuCurrentbg = bgcolorPrev;
+	    	}else if('ctaBackground'  === elementId){
+	    		ctaCurrentbg = bgcolorPrev;
+	    	}else if('footerBackground'  === elementId){
+	    		footerCurrentbg = bgcolorPrev;
+	    	}
+	    		
+	    },
+	    hide: function(color) {
+	    }
+		
+	});
+}
+
+function addMenuElement(menuLinksArray){
+	for (var i=1; i < 5; i++) {
+		var name = $("#menu_elment_name"+i).val();
+		var url  = $("#menu_elment_url"+i).val();
+		if(name != null && name != ''){
+			var menuLink = new Object();
+			menuLink.caption = name;
+			menuLink.url = url;
+			menuLinksArray.push(menuLink);
+		}
+	}
+}
+
+/*function savePreferences() {
+	setLoadingDiv($('body'));
+	 retObj = new Object();
+	 retObj.mandatorName = customerID;
+	 retObj.logo = $("#logo").val();
+	 retObj.holineName = $("#menu_elment_hotline_name").val();
+	 retObj.holineNumber = $("#menu_elment_hotline_number").val();
+	 retObj.background = currentbg;
+	 retObj.ctaBackground = ctaCurrentbg;
+	 retObj.cta = $("#cta").val();
+	 retObj.topic = $("#topic").val();
+	 
+	 
+	 retObj.menuBackground = menuCurrentbg;
+	 retObj.menuLinks = new Array();
+	 addMenuElement(retObj.menuLinks);
+	 
+	 retObj.footerBackground = footerCurrentbg;
+	 retObj.facebook = $("#facebook").val();
+	 retObj.googleplus = $("#googleplus").val();
+	 retObj.twitter = $("#twitter").val();
+	 
+	 $.ajax({
+		  type: "POST",
+		  mimeType: "application/json",
+		  contentType: "application/json;charset=UTF-8",
+		  dataType: "html",
+		  data: JSON.stringify(retObj),
+		  url: "/api/v4/" + encodeURIComponent(customerID) + "/nl2go/get_preview_with_params",
+		  success: function(json) {
+			  unsetLoadingDiv($('body'));
+			  $('#previewFrame').contents().find('html').html(json);
+		  },
+		  error: function() {
+			  unsetLoadingDiv($('body'));
+			  stdAjaxErrorHandler();
+		  }
+	  });	
+}*/
+
+function stdAjaxErrorHandler(jqXHR, textStatus, errorThrown) {
+	  if(jqXHR != null && jqXHR.status == 403) {
+		  setMessagePopUp("problem", "error_server_error_403");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 401) {
+		  setMessagePopUp("problem", "error_server_error_401");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 400) {
+		  setMessagePopUp("problem", "error_server_error_400");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 404) {
+		  setMessagePopUp("problem", "error_server_error_404");
+	  }
+	  else if(jqXHR != null && jqXHR.status == 409) {
+		  setMessagePopUp("problem", "error_server_error_409");
+	  }
+	  else {
+		  setMessagePopUp("problem", "error_server_error");
+	  }
+}
+
+
+
+
+function savePreferences2() {
+	setLoadingDiv($('body'));
+	 retObj = new Object();
+	 retObj.mandatorName = customerID;
+	 retObj.logo = $("#logo").val();
+	 retObj.holineName = $("#menu_elment_hotline_name").val();
+	 retObj.holineNumber = $("#menu_elment_hotline_number").val();
+	 retObj.cta = $("#cta").val();
+	 retObj.topic = $("#topic").val();
+	 
+	 retObj.background = currentbg;
+	 retObj.menuBackground = menuCurrentbg;
+	 retObj.menuLinks = new Array();
+	 addMenuElement(retObj.menuLinks);
+	 
+	 retObj.facebook = $("#facebook").val();
+	 retObj.googleplus = $("#googleplus").val();
+	 retObj.twitter = $("#twitter").val();
+	 
+	 
+	 retObj.hello1 = $("#basket_subject").val();
+	 retObj.note1 = $("#basket_note").val();
+	 retObj.footer1 = $("#basket_footer").val();
+	 retObj.hello2 = $("#click_subject").val();
+	 retObj.note2 = $("#click_note").val();
+	 retObj.footer2 = $("#click_footer").val();
+	 
+	 
+	 var imprintName = $("#imprintName").val();
+	 var imprintURL  = $("#imprintURL").val();
+	 if(imprintName != null && imprintName != ''){
+		 var menuLink = new Object();
+		 menuLink.caption = imprintName;
+		 menuLink.url = imprintURL;
+		 retObj.imprint = menuLink;
+	 }
+	 
+	 var unsubscribeName = $("#unsubscribeName").val();
+	 var unsubscribeURL  = $("#unsubscribeURL").val();
+	 if(imprintName != null && imprintName != ''){
+		 var menuLink = new Object();
+		 menuLink.caption = unsubscribeName;
+		 menuLink.url = unsubscribeURL;
+		 retObj.unsubscribe = menuLink;
+	 }
+	 
+	 $.ajax({
+		  type: "POST",
+		  mimeType: "application/json",
+		  contentType: "application/json;charset=UTF-8",
+		  dataType: "json",
+		  data: JSON.stringify(retObj),
+		  url: "/api/v4/" + encodeURIComponent(customerID) + "/nl2go/pref/savePreferences",
+		  success: function(json) {
+			  unsetLoadingDiv($('body'));
+			  var isrc="/api/v4/" + encodeURIComponent(customerID) + "/nl2go/get_preview";
+			  $('#previewFrame').attr('src',isrc);   
+			  setMessagePopUp("positive", "message_data_saved_successfully");
+		  },
+		  error: function() {
+			  unsetLoadingDiv($('body'));
+			  stdAjaxErrorHandler();
+		  }
+	  });	
+}
+
+
---
+++ ebl2/js/contactup.js
@@ -0,0 +1,124 @@
+var captchascope;
+$(document).ready(function () {
+   
+    var editData =$("#contact");
+    editData.on("click", function(event){
+    	setDialogsContact('messageContact','messageBodyContact','destroy_dialog');
+    });
+    var editData2 =$("#corporate");
+    editData2.on("click", function(event){
+    	setDialogsContact('messageCorporate','messageBodyCorporate','destroy_dialog');
+    	var editData4=$("#copyrightsLink");
+    	editData4.off('click').on("click", function(event){
+    	   	var layerBody = $('#messageCorporate').parent('body');
+    	   	var layer = $('#messageBodyCorporate');
+    	   	var overlay = $('#messageCorporate');
+    	   	closeLayerContact(layerBody, layer, overlay);
+    	   	setDialogsContact('messageCopyrights','messageBodyCopyrights','destroy_dialog');
+    	 });
+    });
+    var editData3 =$("#privacy");
+    editData3.on("click", function(event){
+    	setDialogsContact('messagePrivacy','messageBodyPrivacy','destroy_dialog');
+    	
+    });
+    var editData4 =$("#resetp");
+    if(editData4 != 'undefined'){
+	    editData4.on("click", function(event){
+	    	$('label[for="fpemail"]').parent().removeClass("problem");
+	    	$('label[for="captcha"]').parent().removeClass("problem");
+	    	$('.validation_message').hide();
+	    	captchascope = Math.round(Math.random() * 100000);
+	        $('#captchaimage').attr('src', '/ebl/v3/registration/create_captcha?captchascope=' + captchascope);
+	        $('#captcha').val("");
+	    	setDialogsContact('messageReset','messageBodyReset','destroy_dialog');
+	    	
+	    });
+    }
+   
+});
+
+var openLayerContact = function(overlay, layer, layerBody) {
+	layer.remove();
+	$(layer).appendTo(overlay);
+	overlay.show();
+	layerBody.css({'overflow':'auto'});
+	layer.fadeIn('slow', function(){
+		var scrollTop = $(window).scrollTop();
+		layer.css({'top':scrollTop + 'px'});
+	});
+};
+    	  
+    	  
+var layerSizingContact = function(overlay){
+	var fullWidth = $('body').outerWidth(true);
+	var fullHeight = $('body').outerHeight(true);
+	overlay.css({'height':fullHeight + 'px', 'width':fullWidth + 'px'});
+};
+
+function setDialogsContact(overlayId,dialogBodyId,closeButtonId ) {
+    
+    var overlay = $('#'+overlayId);
+    var layer = $('#'+dialogBodyId);
+    var closeButtonOk = $('#'+closeButtonId);
+    var layerBody = $('#'+overlayId).parent('body');
+    var closeButton = $('.dialog_body .destroy_dialog');
+    
+    openLayerContact(overlay, layer, layerBody);
+    layerSizingContact(overlay);
+    
+    
+    closeButton.click(function() {
+    	closeLayerContact(layerBody, layer, overlay);
+    });
+    
+    closeButtonOk.click(function() {
+    	closeLayerContact(layerBody, layer, overlay);
+    });
+    
+    $(document).bind('keydown', function(e) { 
+      if (e.which == 27) {
+    	  closeLayerContact(layerBody, layer, overlay);
+      }
+    });
+	
+  }
+
+function setDialogsContact(overlayId,dialogBodyId,closeButtonId,closebuttonid2 ) {
+    
+    var overlay = $('#'+overlayId);
+    var layer = $('#'+dialogBodyId);
+    var closeButtonOk = $('#'+closeButtonId);
+    var closeButtonOk2 = $('#'+closebuttonid2);
+    var layerBody = $('#'+overlayId).parent('body');
+    var closeButton = $('.dialog_body .destroy_dialog');
+    
+    openLayerContact(overlay, layer, layerBody);
+    layerSizingContact(overlay);
+    
+    
+    closeButton.click(function() {
+    	closeLayerContact(layerBody, layer, overlay);
+    });
+    
+    closeButtonOk2.click(function() {
+    	closeLayerContact(layerBody, layer, overlay);
+    });
+    
+    closeButtonOk.click(function() {
+    	closeLayerContact(layerBody, layer, overlay);
+    });
+    
+    $(document).bind('keydown', function(e) { 
+      if (e.which == 27) {
+    	  closeLayerContact(layerBody, layer, overlay);
+      }
+    });
+	
+  }
+
+var closeLayerContact = function(layerBody, layer, overlay){
+    layerBody.css({'overflow':'auto'});
+    layer.fadeOut('fast');
+    overlay.hide();
+  };
